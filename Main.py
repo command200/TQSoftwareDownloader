7aa55ff920f1e097a6f39d07430a2002cf898a89
command200 <185820976@qq.com>
2023-07-25 16:14:47 +0800

    以管理员打开

--- Main.py
+++ Main.py
@@ -3,10 +3,23 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QPixmap, QIcon
 import sys
 import os
+import ctypes
+
+# auto-py-to-exe
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    mainWin = MainWin2.MainWin()
-    mainWin.setWindowIcon(QIcon(QPixmap(str(os.path.dirname(__file__))+"/ico.png")))
-    mainWin.show()
-    sys.exit(app.exec_())
+    if is_admin():
+        app = QApplication(sys.argv)
+        mainWin = MainWin2.MainWin()
+        mainWin.setWindowIcon(QIcon(QPixmap(str(os.path.dirname(__file__)) + "/images/ico.png")))
+        mainWin.show()
+        sys.exit(app.exec_())
+    else:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+
+
